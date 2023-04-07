eb13ce2e412859fb9601ec19538504495457c4a9
Matyáš Caras <matyas@caras.cafe>
2023-04-07 18:22:13 +0200

    docs: upravit příklad

--- example/canteenlib_example.dart
+++ example/canteenlib_example.dart
@@ -1,20 +1,15 @@
 import 'package:canteenlib/canteenlib.dart';
 
-void main(List<String> args) {
-  Canteen c = Canteen("https://kantyna.neco.cz");
-  c.login("uzivatel", "heslo").then((value) {
-    c.jidelnicekDen(den: DateTime.parse("2022-04-04")).then((t) async {
-      print((await c.ziskejUzivatele()).kredit);
-      c.objednat(t.jidla[0]).then(
-        (value) {
-          t.jidla[0] = value; // divně udělané ale nic lepšího teď nevymyslím
-          print(t.jidla[0].objednano);
-          print(t.jidla[0].orderUrl);
-        },
-      );
-    });
-  }).catchError((o) {
-    print(o);
-    return null;
-  });
+void main(List<String> args) async {
+  Canteen c = Canteen(
+      "https://kantyna.neco.cz"); // vytvořit instanci kantýny, všechna komunikace probíhá skrz ni
+  try {
+    await c.login("uzivatel", "heslo"); // přihlásit se
+    var jidelnicek = await c.jidelnicekDen(den: DateTime.parse("2022-04-04"));
+    print((await c.ziskejUzivatele()).kredit);
+    var objednano = await c.objednat(jidelnicek.jidla[0]);
+    print(objednano.objednano);
+  } catch (e) {
+    print("Při získávání informací nastala chyba: $e");
+  }
 }
